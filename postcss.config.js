3f67645a4bc55c73e1a53396ad891554d134975f
Mathias Maciossek <mathias.maciossek@gmail.com>
2020-03-28 14:47:04 +0100

    ⬆️ Update dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
+  parser: "postcss-scss",
   plugins: {
-    'postcss-import': {},
-    'postcss-cssnext': {},
-    'cssnano': {}
+    "postcss-import": {},
+    "postcss-cssnext": {}
   }
-}
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,88 @@
-const resolve = require('path').resolve
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const resolve = require("path").resolve;
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = function(env) {
   const config = {
     entry: {
       main: [
-        'webpack-dev-server/client?http://0.0.0.0:3001',
+        "webpack-dev-server/client?http://0.0.0.0:3001",
         // bundle the client for webpack-dev-server
         // and connect to the provided endpoint
 
-        './index.js'
+        "./index.js"
         // the entry point of our app
       ]
     },
     output: {
-      filename: '[name].[hash].js',
-      chunkFilename: '[name].[hash].js',
+      filename: "[name].[hash].js",
+      chunkFilename: "[name].[hash].js",
       // the output bundle
 
-      path: resolve(__dirname, './dist'),
+      path: resolve(__dirname, "./dist"),
 
-      publicPath: '/'
+      publicPath: "/"
       // necessary for HMR to know where to load the hot update chunks
     },
 
-    context: resolve(__dirname, './src'),
-    devtool: 'source-map',
+    context: resolve(__dirname, "./src"),
+    devtool: "source-map",
     watch: false,
     devServer: {
-      contentBase: resolve(__dirname, './dist'),
+      contentBase: resolve(__dirname, "./dist"),
       // match the output path
 
-      publicPath: '/'
+      publicPath: "/"
       // match the output `publicPath`
     },
 
     module: {
       rules: [
         {
-          test: /\.scss$/,
-          use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: [
-              'css-loader?sourceMap&importLoaders=1',
-              'postcss-loader',
-              'sass-loader'
-            ]
-          }))
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                hmr: devMode
+              }
+            },
+            "css-loader",
+            "postcss-loader",
+            "sass-loader"
+          ]
         }
       ]
     },
 
     plugins: [
       new HtmlWebpackPlugin({
-        template : resolve(__dirname, './src/index.html'),
-        hash     : false,
-        filename : 'index.html',
-        inject   : 'body',
-        minify   : {
-          collapseWhitespace : true
+        template: resolve(__dirname, "./src/index.html"),
+        hash: false,
+        filename: "index.html",
+        inject: "body",
+        minify: {
+          collapseWhitespace: true
         }
       }),
-      new ExtractTextPlugin({ filename: 'bundle.css', disable: false, allChunks: true }),
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: devMode ? "[name].css" : "[name].[hash].css",
+        chunkFilename: devMode ? "[id].css" : "[id].[hash].css"
+      }),
       new webpack.ProvidePlugin({
-        'jQuery': 'jquery',
-        'window.jQuery': 'jquery',
-        'Tether': 'tether',
-        'window.Tether': 'tether',
-        Popper: ['popper.js', 'default'],
+        jQuery: "jquery",
+        "window.jQuery": "jquery",
+        Tether: "tether",
+        "window.Tether": "tether",
+        Popper: ["popper.js", "default"]
       })
     ]
-  }
+  };
 
-  return config
-}
+  return config;
+};
